182250a12b959a4b03968413d6e03c9ac3a2628d
bornittah <bornittah@gmail.com>
2023-02-18 22:31:11 +0300

    Fix linters errors

--- person.rb
+++ person.rb
@@ -4,38 +4,38 @@ require_relative 'trimmer'
 require_relative 'base_decorator'
 require_relative 'rental'
 
-  class Person < Nameable
-    attr_accessor :name, :age
-    attr_reader :id, :rentals
-  
-    def initialize(age:, name: 'Unkown', parent_permission: true)
-      super()
-      @id = Random.rand(1..1000)
-      @name = name
-      @parent_permission = parent_permission
-      @age = age
-      @rentals = []
-    end
-  
-    def of_age?
-      return true if @age >= 18
-  
-      false
-    end
-  
-    def can_use_services
-      return true if of_age? || @parent_permission
-  
-      false
-    end
-  
-    def correct_name
-      @name
-    end
-  
-    def add_rental(book, date)
-      Rental.new(date, book, self)
-    end
-  
-    private :of_age?
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age:, name: 'Unkown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @parent_permission = parent_permission
+    @age = age
+    @rentals = []
   end
+
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
+
+  def can_use_services
+    return true if of_age? || @parent_permission
+
+    false
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
+  private :of_age?
+end
